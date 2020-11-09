2004427fbf9b19927aab47305a1cb4ac3f5f0fc0
Arik Smith <arik@ariks-air.lan>
2020-11-08 19:56:12 -0500

    clean up code and remove unneeded styles

--- index.js
+++ index.js
@@ -1,46 +1,7 @@
-    var tipPercentage = 0.15;
-    var tipPercentageB = 0.20;
-    var tipPercentageC = 0.25;
-    
-    //the math for 15%
-    const calculateTip = () => {
-        const bill = document.getElementById("bill").value;
-        const tipAmount = bill * tipPercentage; 
-       console.log("Bill:", bill);
-       console.log("Tip Percentage:", tipPercentage);
-       console.log(tipAmount);
-       document.getElementById("demo").innerHTML = ("You Should Tip $" +(tipAmount));
-      // window.alert("You should tip $"+ (tipAmount));
-       // TODO: Figure out how to print the tipAmount appear in HTML
-    }
-
-     //the math for 20%
-     const calculateTipB = () => {
-        const bill = document.getElementById("bill").value;
-        const tipAmount = bill * tipPercentageB; 
-       console.log("Bill:", bill);
-       console.log("Tip Percentage:", tipPercentageB);
-       console.log(tipAmount);
-       document.getElementById("demo").innerHTML = ("You Should Tip $" +(tipAmount));
-       //window.alert("You should tip $"+ (tipAmount));
-       // TODO: Figure out how to print the tipAmount appear in HTML
-    }
-
-    //the math for 25%
-    const calculateTipC = () => {
-        const bill = document.getElementById("bill").value;
-        const tipAmount = bill * tipPercentageC; 
-       console.log("Bill:", bill);
-       console.log("Tip Percentage:", tipPercentageC);
-       console.log(tipAmount);
-       document.getElementById("demo").innerHTML = ("You Should Tip $" +(tipAmount));
-      // window.alert("You should tip $"+ (tipAmount));
-       // TODO: Figure out how to print the tipAmount appear in HTML
-    }
-
-    /*
-    TODO: Figure out how to use this function in the html to update the tip variable
-    */
-    const changeTip = (newPercentage) => {
-        tipPercentage = newPercentage;
-    }
+// Calculates tip based on the given percent
+const calculateTip = (percent) => {
+  const bill = document.getElementById("bill").value;
+  const tipPercentage = percent / 100;
+  const tipAmount = bill * tipPercentage;
+  document.getElementById("tip").innerHTML = `You Should Tip $${tipAmount}`;
+};
