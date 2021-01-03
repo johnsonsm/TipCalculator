38835ddc2cace809f6e94a191f2a78b3f86781d2
4upz <ams382@case.edu>
2021-01-03 16:21:14 -0500

    Complete redesign

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+let currentPercentage;
+
 // Calculates tip based on the given percent
 const calculateTip = (percent) => {
+  setCurrentPercentage(percent.toString());
   const bill = document.getElementById("bill").value;
   const tipPercentage = percent / 100;
-  const tipAmount = bill * tipPercentage;
-  document.getElementById("tip").innerHTML = `You Should Tip $${tipAmount}`;
+  const tipAmount = (bill * tipPercentage).toFixed(2);
+  document.getElementById("tip").innerHTML = `$${tipAmount}`;
+};
+
+// Saves the current tip percentage being shown, and highlights the button for it
+const setCurrentPercentage = (newPercentage) => {
+  if (currentPercentage) {
+    document.getElementById(currentPercentage).className = "";
+  }
+  currentPercentage = newPercentage;
+  document.getElementById(currentPercentage).className = "current-percentage";
 };
